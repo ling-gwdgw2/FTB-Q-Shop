a105ed255aa71d8afcb8314ecfe027ddf10cc027
Rube <vivo99621@gmail.com>
2026-08-01 00:44:04 +0700

    Auto-register InventoryButtonListener using @EventBusSubscriber annotation for instant inventory button rendering

--- com/holysweet/questshop/client/InventoryButtonListener.java
+++ com/holysweet/questshop/client/InventoryButtonListener.java
@@ -5,16 +5,19 @@ import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.Tooltip;
 import net.minecraft.client.gui.screens.inventory.InventoryScreen;
 import net.minecraft.network.chat.Component;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.ScreenEvent;
 
+@EventBusSubscriber(modid = "questshop", value = Dist.CLIENT)
 public class InventoryButtonListener {
 
     @SubscribeEvent
     public static void onScreenInit(ScreenEvent.Init.Post event) {
         if (event.getScreen() instanceof InventoryScreen inventoryScreen) {
-            int x = InventoryScreenHelper.getLeft(inventoryScreen) + 150;
-            int y = InventoryScreenHelper.getTop(inventoryScreen) + 60;
+            int x = InventoryScreenHelper.getLeft(inventoryScreen) + 140;
+            int y = InventoryScreenHelper.getTop(inventoryScreen) + 6;
             Button shopBtn = Button.builder(
                 Component.literal("Shop"),
                 btn -> {
@@ -22,7 +25,7 @@ public class InventoryButtonListener {
                         Minecraft.getInstance().player.connection.sendCommand("shop");
                     }
                 }
-            ).bounds(x, y, 32, 18).tooltip(Tooltip.create(Component.literal("FtbQshop (เปิดร้านค้า)"))).build();
+            ).bounds(x, y, 32, 16).tooltip(Tooltip.create(Component.literal("FtbQshop (เปิดร้านค้า)"))).build();
             event.addListener(shopBtn);
         }
     }
